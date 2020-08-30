4f45fd33109ca08a8c56298cfc26c224a01c2738
Ultrayano <ultrayanoo@gmail.com>
2020-08-30 19:10:10 +0200

    Typo

--- src/java/management/Main.java
+++ src/java/management/Main.java
@@ -43,7 +43,7 @@ public class Main extends Application {
     public void showMainView() {
         try {
             FXMLLoader loader = new FXMLLoader();
-
+            loader.setLocation(Main.class.getResource("../resources/fxml/MainView.fxml"));
             
             // Show the scene containing the root layout.
             Scene scene = new Scene(loader.load());
